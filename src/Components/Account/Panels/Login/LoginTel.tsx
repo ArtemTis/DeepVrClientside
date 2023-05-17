0408ab7ff4e16e3a0176ec8e027982ca8b2b09d0
Artem Tishenko <artem.200207@yandex.ru>
2023-05-18 00:01:30 +0400

    createAsyncThunk

--- src/Components/Account/Panels/Login/LoginTel.tsx
+++ src/Components/Account/Panels/Login/LoginTel.tsx
@@ -3,6 +3,10 @@ import { PassField } from '../../../Common/FormFields/PassField'
 import { useForm } from 'react-hook-form';
 import { ILoginForm } from '../../../../Utils/types';
 import { NextButton } from '../../../Common/Markup/NextButton';
+import { useState } from 'react';
+import { Api } from '../../../../Utils/api';
+import { useAppDispatch } from '../../../../Utils/redux/store';
+import { setToken, setUser } from '../../../../Utils/redux/authSlice';
 
 const LoginTel = () => {
 
@@ -13,6 +17,41 @@ const LoginTel = () => {
     } = useForm<ILoginForm>({
         mode: "onTouched",
     });
+
+    const dispatch = useAppDispatch();
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [reqError, setReqError] = useState<string>();
+
+    const onLoginClick = () => {
+        setReqError("");
+        setIsLoading(true);
+        
+
+        Api.login(getValues())
+            .then((res) => {
+                console.log(res);
+                if (Api.checkStatus(res)) {
+                    if (!!res.data && !res.data.error) {
+                        dispatch(setToken(res.data.token));
+                        dispatch(setUser(res.data.user));
+                    } else {
+                        setReqError(
+                            (res.data.error_text as string) ?? "Ошибка авторизации"
+                        );
+                    }
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                if (!!err.data?.error) {
+                    setReqError(err.data.error_text);
+                } else if (err.response.status >= 500)
+                    setReqError("Ошибка сервера, попробуйте позже");
+            })
+            .finally(() => setIsLoading(false));
+
+    };
 
     return (
         <>
@@ -30,7 +69,7 @@ const LoginTel = () => {
                 autocomplete="current-password"
                 unregister
             />
-            <NextButton isActive={isValid}>
+            <NextButton isActive={isValid} onClick={onLoginClick}>
                 Войти
             </NextButton>
         </>
--- src/Utils/redux/authSlice.ts
+++ src/Utils/redux/authSlice.ts
@@ -1,16 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { ICity, IUser } from "../types";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { ICity, ILoginForm, IUser } from "../types";
 import { RootState } from "./store";
+import { Api } from "../api";
 
 interface AuthState {
     token: string;
     user?: IUser;
     selectedCity?: ICity;
+    textError?: string;
+    reqStatus: string;
 }
 
-const initialState : AuthState = {
-    token: ''
+const initialState: AuthState = {
+    token: '',
+    reqStatus: ''
 };
+
+export const singIn = createAsyncThunk(
+    'authSlice/singIn',
+    async function (getValues: ILoginForm,{rejectWithValue, dispatch}) {
+        await Api.login(getValues)
+            .then((res) => {
+                
+                if (Api.checkStatus(res)) {
+                    if (!!res.data && !res.data.error) {
+                        dispatch(setToken(res.data.token));
+                        dispatch(setUser(res.data.user));
+                    } else {
+                        dispatch(setErrorText(
+                            (res.data.error_text as string) ?? "Ошибка авторизации"
+                        ));
+                    }
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                if (!!err.data?.error) {
+                    dispatch(setErrorText(err.data.error_text));
+                } else if (err.response.status >= 500)
+                    dispatch(setErrorText("Ошибка сервера, попробуйте позже"));
+            })
+            // .finally(() => setIsLoading(false));
+    }
+)
 
 const bookingSlice = createSlice({
     name: 'authSlice',
@@ -19,20 +51,38 @@ const bookingSlice = createSlice({
         setToken: (state, action) => {
             state.token = action.payload;
         },
-        setUser: (state, action : {payload: IUser|undefined}) => {
+        setUser: (state, action: { payload: IUser | undefined }) => {
             state.user = action.payload;
         },
-        setSelectedCity(state, action : {payload: ICity|undefined}) {
+        setSelectedCity: (state, action: { payload: ICity | undefined }) => {
             state.selectedCity = action.payload;
+        },
+        setErrorText: (state, action) => {
+            state.textError = action.payload;
+        },
+        setReqStatus: (state, action) => {
+            state.reqStatus = action.payload;
         }
-    }
+    },
+    // extraReducers: {
+    //     [singIn.pending]: (state, action) => {
+    //         state.status = 'loading';
+    //         state.textError = null;
+    //     },
+    //     [singIn.fulfilled]: (state, action) => {
+    //         state.status = 'resolve';
+    //     },
+    //     [singIn.rejected]: (state, action) => {},
+    // }
 });
 
 export const getToken = (state: RootState) => state.authReducer.token;
 export const getIsAuthorised = (state: RootState) => !!state.authReducer.token;
 export const getUser = (state: RootState) => state.authReducer.user;
 export const getSelectedCity = (state: RootState) => state.authReducer.selectedCity;
+export const getReqStatus = (state: RootState) => state.authReducer.reqStatus;
+export const getTextErrors = (state: RootState) => state.authReducer.textError;
 
 
-export const { setToken, setUser, setSelectedCity } = bookingSlice.actions;
+export const { setToken, setUser, setSelectedCity, setErrorText, setReqStatus } = bookingSlice.actions;
 export default bookingSlice.reducer;
